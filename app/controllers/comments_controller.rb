54ec8484f6071c77f9ce77f5a392dfe6c6c8b7f9
kasiaya <ayano.kashimoto@gmail.com>
2024-08-16 12:06:43 +0900

    修正7_コメント機能5

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to prototype_path(@comment.prototype) # 今回の実装には関係ありませんが、このようにPrefixでパスを指定することが望ましいです。
     else
-      @prototype = @comment.prototype # 必要なインスタンス変数を設定
+      @prototypes = @comment.prototype # 必要なインスタンス変数を設定
       @comments = @prototype.comments # プロトタイプに紐付いたコメントも表示させるために、これを追加します
       render 'prototypes/show'
     end
